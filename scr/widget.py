1d36406558d27b6d320b0bab6bafbd6ef5bdeb56
Степан Прокопьев <dr.aibolitarise@mail.ru>
2025-09-16 00:49:25 +0900

    widget.py

--- scr/widget.py
+++ scr/widget.py
@@ -1,5 +1,8 @@
+from  masks import get_mask_account, get_mask_card_number  # Импортируем функции из masks.py
+from datetime import datetime
+
+
 def mask_account_card(input_string: str) -> str:
-    from masks import get_mask_account, get_mask_card_number  # Импортируем функции из masks.py
     """
     Маскирует номер карты или счета в зависимости от типа
     """
@@ -27,24 +30,16 @@ def mask_account_card(input_string: str) -> str:
     except ValueError:
         raise ValueError("Ошибка при обработке номера")
 
-from datetime import datetime
-
 
 def get_date(date_string: str) -> str:
     """
     Преобразует строку с датой из формата ISO в формат ДД.ММ.ГГГГ
-
-    Args:
-    date_string (str): Строка с датой в формате "2024-03-11T02:26:18.671407"
-
-    Returns:
-    str: Дата в формате "ДД.ММ.ГГГГ"
     """
     try:
-    # Парсим исходную дату
-            original_date = datetime.fromisoformat(date_string)
-    # Форматируем в нужный формат
-            formatted_date = original_date.strftime('%d.%m.%Y')
-            return formatted_date
+        # Парсим исходную дату
+        original_date = datetime.fromisoformat(date_string)
+        # Форматируем в нужный формат
+        formatted_date = original_date.strftime('%d.%m.%Y')
+        return formatted_date
     except ValueError:
         raise ValueError("Неверный формат даты. Ожидается формат 'ГГГГ-ММ-ДДТЧЧ:ММ:СС'")
