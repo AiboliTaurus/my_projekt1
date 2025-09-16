b6e4825c1178a0a42b23c0f259af8fc296b31952
Степан Прокопьев <dr.aibolitarise@mail.ru>
2025-09-16 21:21:06 +0900

    widget.py

--- scr/widget.py
+++ scr/widget.py
@@ -1,4 +1,4 @@
-from  masks import get_mask_account, get_mask_card_number  # Импортируем функции из masks.py
+from masks import get_mask_account, get_mask_card_number  # Импортируем функции из masks.py
 from datetime import datetime
 
 
